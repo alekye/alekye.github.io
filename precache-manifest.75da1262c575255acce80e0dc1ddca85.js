d4c8aa54ccbfd3313557eeb7d4359759e513dbc3
Alek Ye <alek.ye@smartaid.cn>
2019-07-25 17:13:05 +0800

    Add react home page

---
+++ precache-manifest.75da1262c575255acce80e0dc1ddca85.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05ef21b283189c033cc071443561ec20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "198ce3431d133729d417",
+    "url": "/static/css/main.bf9d35e1.chunk.css"
+  },
+  {
+    "revision": "9634a2804d5597ac62b3",
+    "url": "/static/js/2.548e0644.chunk.js"
+  },
+  {
+    "revision": "198ce3431d133729d417",
+    "url": "/static/js/main.f67c748b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "36456d61d04563103f3fe77e1e93d773",
+    "url": "/static/media/img_about.36456d61.jpg"
+  },
+  {
+    "revision": "431500b39e61a226c8fde50933eaa9b6",
+    "url": "/static/media/img_home.431500b3.jpg"
+  },
+  {
+    "revision": "06dc99e979cf960a35b8ba6c8d652083",
+    "url": "/static/media/logo_home.06dc99e9.jpg"
+  }
+]);
